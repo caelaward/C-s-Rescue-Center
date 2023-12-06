66a537fe8fed41d7b76b5c474422605db32ef427
caelaward <146817394+caelaward@users.noreply.github.com>
2023-12-06 16:41:57 +0200

    added to checkout.js

---
+++ script/checkout.js
@@ -0,0 +1,76 @@
+cart=JSON.parse(localStorage.getItem('cart'))
+
+let displayCheckout=document.querySelector('[data-checkout]')
+function checkoutDisplay(){
+    let p = cart.map(function(item,index) {
+        // console.log(item); 
+        // console.log(index);
+        return`
+        <thead>
+        <tr>
+            <th scope="col"></th>
+          <th scope="col">Item</th>
+          <th scope="col">Description</th>
+          <th scope="col">price</th>
+          <th scope="col">img</th>
+          <th scope="col">Q</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+        <td>${index+1}</td>
+          <td>${item.name}</td>
+          <td>${item.description}</td>
+          <td>R${item.price}</td>
+          <td><img src="${item.url}" id="img-table"></td>
+          <td></td>
+          <td><button class="delete" value=${index}>X</button></td>
+          </tr>
+    <tbody>
+
+       
+
+            `
+    })
+    // join will join the trs together 
+    displayCheckout.innerHTML=p.join('')
+}
+checkoutDisplay()//will ensure that the function still running
+
+let deleteButton=document.querySelector('.delete')
+
+displayCheckout.addEventListener('click',function () {
+    //event.target will pick up anything clicked inside the parent tag (from the children)
+    if(event.target.classList.contains('delete')) {
+        //event.target is the button
+        removeP(event.target.value) //passing value set to button
+        alert("Are you sure you want to delete this item")
+    }
+
+})
+
+function removeP(position) {
+    cart.splice(position,1)//right now event.target and positionhas the same value
+    updateData()//function to set data and get it
+    checkoutDisplay()//function to view.. function to loop through array
+
+}
+
+function updateData() {
+    localStorage.setItem('cart',JSON.stringify(cart))
+    cart=JSON.parse(localStorage.getItem('cart'))
+}
+
+
+function removeDuplicates(cart) {
+    let unique = [];
+    cart.forEach(item => {
+        if (!unique.includes(item)) {
+            unique.push(item);
+        }
+    });
+    return unique;
+}
+
+removeDuplicates(cart)
--- script/product.js
+++ script/product.js
@@ -1,109 +1,46 @@
 
 products=JSON.parse(localStorage.getItem('products'))
-// let cart=[]//everything being purchased must be added here
+let cart=[]//everything being purchased must be added here
+
 
 let items=document.querySelector('[data-items]')
 
-// let c=products.map(function (item,index) {
-//     return `
-//         <div class="col">
-//             <div class="card h-100 w-75">
-//                 <img src="${item.url}" class="card-img-top " >
-//                     <div class="card-body">
-//                         <h5 class="card-title">${item.name}</h5>
-//                         <p class="card-text">${item.description}.</p>
-//                         <p class="price">R${item.price}</p>
-//                         <button 'class='w-50' id='btnAdd' data-add>Add to cart</button>
-//                     </div>
-//              </div>
-//          </div>
-    
-//     `
-// }).join('')
-
-    
-//     items.innerHTML=c
 
 
-
-
-//sort
-
-
-
-// sortbtn.addEventListener('click', )
-
-// let compPrice =(a,b)=>a.price-b.price;
-// let sortedPrice=products.sort(compPrice) 
-// // c(sortedPrice)
-// console.log(sortedPrice)
-
-// sortbtn.addEventListener('click',function () {
-    //     alert('hi')
-    //     products.sort((a, b) => a.price - b.price);
-    //     return 
-    // })
-    
-    // function sortPrice() {
-            //     let sortedPrice=[...products].sort((a,b)=>{
-                //         return parseInt(a.price)-parseInt(b.price)
-                    //         console.log(sortedPrice);
-                    //     });
-                    // if (sortPrice) {
-                        //     items.innerHTML=sortedPrice
-                        // }
-                        // }
-                        // sortbtn.addEventListener('click', sortPrice)
-                        
-                        
-                        // let searchbtn=document.querySelector('[data-search]')
-                        //search function
-                        
-                        //                         let inputV=document.querySelector('[data-input]')
-                        //                         //addevent
-                        //                         inputV.addEventListener("keyup", () => {
-                            //                             let SearchItem = products.filter((prod) => {
-                                //     return prod.name.toLowerCase().includes(inputV.value.toLowerCase());
-                                //   });
-                                //   if (SearchItem) {
-                                    //   items.innerHTML=("SearchItem");
-                                    //   }
-                                    // });
-                                    
-                                    function productDisplay(prod){
-                                        let p = prod.map(function(item,index) {
-                                            console.log(item); 
-                                            return`
-                                                    <div class="col">
-                                                        <div class="card h-100 w-75">
-                                                            <img src="${item.url}" class="card-img-top " >
-                                                            <div class="card-body">
-                                                            <h5 class="card-title">${item.name}</h5>
-                                                            <p class="card-text">${item.description}.</p>
-                                                            <p class="price">R${item.price}</p>
-                                                            <button 'class='w-50' id='btnAdd' data-add>Add to cart</button>
-                                                            </div>
-                                                        </div>
-                                                     </div>
+       function productDisplay(prod){
+              let p = prod.map(function(item,index) {
+                    console.log(item);
+                        return`
+                             <div class="col">
+                                <div class="card h-100 w-75">
+                                         <img src="${item.url}" class="card-img-top " >
+                                            <div class="card-body">
+                                                <h5 class="card-title">${item.name}</h5>
+                                                <p class="card-text">${item.description}.</p>
+                                                <p class="price">R${item.price}</p>
+                                                <button 'class='w-50' id='btnAdd' value=${index} data-add>Add to cart</button>
+                                            </div>
+                                          </div>
+                                        </div>
                                                         `
-                                                    })
-                                                    //join will join the trs together 
-                                                    items.innerHTML=p.join('')
-                                                }
-                                                productDisplay(products)
-         
+                                          })
+                                         //join will join the trs together
+                                         items.innerHTML=p.join('')
+                                      }
+                                      productDisplay(products)
+
 let sortbtn=document.querySelector('[data-sort]')
 
 // let compPrice= (a,b)=> {
-//         return a.price-b.price;  
+//         return a.price-b.price;
 //     }
-    
+
 //     let sortedPrice=products.sort(compPrice)
-    
+
 //     console.log(sortedPrice);
-    
-    
-   
+
+
+
 // function sortPrice() {
 //     let sortedPrice=products.sort((a,b)=>{
 //         return parseInt(a.price) - parseInt (b.price);
@@ -129,12 +66,12 @@ function sortPrice(event) {
 
 }
 
-            
+
  //search function
  let inputV=document.querySelector('[data-input]')
 function find() {
     let findItem =document.querySelector('[data-input]').value.toLowerCase()
-    
+
     let searchFilter=products.filter(item=>{
         return item.name.toLowerCase().includes(findItem)
     });
@@ -146,7 +83,7 @@ inputV.addEventListener('keyup',find)
 // searchbtn.addEventListener('click',function () {
 //     alert('Item searched will be displayed')
 //     find()
-    
+
 // })
 
 
@@ -166,4 +103,18 @@ if (products.length===0) {
     productDisplay(products)
 }
 
-  
+function add(index) {
+    cart.push(products[index])
+    localStorage.setItem('cart',JSON.stringify(cart))
+
+}
+// add event listener on parent
+items.addEventListener('click',function (event) {
+    if (event.target.hasAttribute('data-add')) {
+        alert('button pressed')
+        add(event.target.value)
+    }
+
+})
+
+// localStorage.setItem
